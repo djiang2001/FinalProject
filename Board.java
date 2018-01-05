f65ee6b415cf0bf75ad86249eab5f3ebf20567d4
djiang2 <djiang2>
2018-01-04 21:12:59 -0500

    Created a GUI display

--- Board.java
+++ Board.java
@@ -1,6 +1,20 @@
-public class Board{
+import java.awt.*;
+import javax.swing.*;
+public class Board extends JFrame{
 
-    private void createBoard(){
-	System.out.println("hi");
-    }
+  public Board(){
+    this.setTitle("Deblocked");
+    this.setSize(600,600);
+    this.setLocation(550,150);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+  }
+
+
+  public static void main (String[] args )
+  {
+    Board frame  = new Board();
+
+    frame.setVisible(true );
+
+  }
 }
