8fac3db29a0ee7f6bbddaff8705632a668a8354d
djiang2 <djiang2>
2018-01-05 15:19:00 -0500

    Made resizable and tried to make a box layout

--- Board.java
+++ Board.java
@@ -5,27 +5,36 @@ import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 public class Board extends JFrame{
+    private Container pane;
 
-  public Board(){
-    this.setTitle("Deblocked");
-    this.setSize(600,600);
-    this.setLocation(550,150);
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public Board(){
+	this.setTitle("Deblocked");
+	this.setSize(600,600);
+	this.setLocation(550,150);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-    addMouseListener(new MouseAdapter() {
-      public void mousePressed(MouseEvent demo){
-        System.out.println("hello");
-      }
+	
+
+	addMouseListener(new MouseAdapter() {
+		public void mousePressed(MouseEvent demo){
+		    System.out.println("hello");
+		}
+	    });
+
+	pane = this.getContentPane();
+	pane.setLayout(new BoxLayout(pane, BoxLayout.LINE_AXIS));
+
+	
+	pane.add(Box.createHorizontalGlue());
+	pane.add(Box.createRigidArea(new Dimension(10, 0)));
     }
-      );
-  }
 
 
-  public static void main (String[] args )
-  {
-    Board frame  = new Board();
-
-    frame.setVisible(true );
-
-  }
+    public static void main (String[] args )
+    {
+	Board game = new Board();
+	game.setAlwaysOnTop(true);
+	game.setVisible(true);
+	game.setResizable(false);
+    }
 }
