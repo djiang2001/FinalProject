1f8da9982260caebecc7b7d0149f5f9289e78662
aimran <aimran@stuycs.org>
2018-01-05 15:31:08 -0500

    Created blocks class

---
+++ Blocks.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Blocks{
+    private String color;
+    private String[] colorColl = {"blue", "green", "yellow", "red", "orange"};
+    private int ycor;
+    private int xcor;
+
+    public Blocks(){
+	for (int i = 0; i < 10; i++){
+	    for (int j = 0; j < 10; j++){
+		color = "fix this";
+	    }
+	}
+    }
+}
