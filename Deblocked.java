9ff82c2eda4c09bcaf22ba2596814f5dae512d4e
djiang2 <djiang2>
2018-01-06 17:27:44 -0500

    Added Score JLabel

--- Deblocked.java
+++ Deblocked.java
@@ -5,36 +5,28 @@ import java.awt.event.MouseEvent;
 import javax.swing.*;
 
 public class Deblocked extends JFrame{
-    private Container pane;
 
-    public Board(){
-	this.setTitle("Deblocked");
-	this.setSize(600,600);
-	this.setLocation(550,150);
-	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+  JLabel score;
+  
+  public Deblocked(){
+    this.setTitle("Deblocked");
+    this.setSize(600,600);
+    this.setLocation(550,150);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	
+    score = new JLabel("0");
+    add(score, BorderLayout.SOUTH);
+  }
 
-	addMouseListener(new MouseAdapter() {
-		public void mousePressed(MouseEvent demo){
-		    System.out.println("hello");
-		}
-	    });
-
-	pane = this.getContentPane();
-	pane.setLayout(new BoxLayout(pane, BoxLayout.LINE_AXIS));
-
-	
-	pane.add(Box.createHorizontalGlue());
-	pane.add(Box.createRigidArea(new Dimension(10, 0)));
-    }
-
-
-    public static void main (String[] args )
-    {
-	Deblocked game = new Deblocked();
-	game.setAlwaysOnTop(true);
-	game.setVisible(true);
-	game.setResizable(false);
-    }
+  public JLabel getScore(){
+    return score;
+  }
+  
+  public static void main (String[] args )
+  {
+    Deblocked game = new Deblocked();
+    game.setAlwaysOnTop(true);
+    game.setVisible(true);
+    game.setResizable(false);
+  }
 }
