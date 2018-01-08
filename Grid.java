b61de9d1c3765f689301b059865e88433acda3ef
djiang2 <djiang2>
2018-01-07 23:31:02 -0500

    Added difficulty and moves based on difficulty

--- Grid.java
+++ Grid.java
@@ -2,15 +2,24 @@ public class Grid implements {
   public int len;
   public int wid;
   public Block[][] content = new Block[5][5];
+  public int moves;
 
   public Grid(){
     makeGrid();
     resetGrid();
   }
 
-  public  void makeGrid(){
+  public  void makeGrid(int difficulty){
     len = 10;
     wid = 10;
+    if(difficulty == 1){
+      moves = 30
+        } else if(difficulty == 2){
+      moves = 25
+        } else {
+      moves = 20
+    }
+
   }
 
   public void resetGrid(){
