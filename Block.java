c861cf9dba1e391d34ae426e6efe16d1c350a3b9
ahmed <aimran@stuy.edu>
2018-01-12 01:49:44 -0500

    Working on displaying icons

--- Block.java
+++ Block.java
@@ -3,47 +3,50 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Block extends JLabel{
-    private String color;
-    private String[] colorColl = {"blue", "green", "yellow", "red", "orange"};
-    private int ycor;
-    private int xcor;
-    private Icon icon;
-    
-    public Block(int color, int ycor, int xcor ){
-	this.color = colorColl[color];
-	this.ycor = ycor;
-	this.xcor = xcor;
-	setIcon(this.color);
-    }
-    
-    //-------------------Getters and Setters--------------------
-    
-    public int getY(){
-	return ycor;
-    }
-    public int getX(){
-	return xcor;
-    }
-    public String getColor(){
-	return color;
-    }
-    public Icon getIcon(){
-	return icon;
-    }
-    
-    public void setY(int y){
-	ycor = y;
-    }
-    public void setX(int x){
-	xcor = x;
-    }
-    public void setColor(String c){
-	color = c;
-    }
-    public void setIcon(String c){
-	icon = new ImageIcon(c+".png");
-    }
-    
-    //------------------Icon Display------------------
-    
+  private String color;
+  private String[] colorColl = {"blue", "green", "yellow", "red", "orange"};
+  private int ycor;
+  private int xcor;
+  private Icon icon;
+  
+  public Block(int color, int ycor, int xcor ){
+    this.color = colorColl[color];
+    this.ycor = ycor;
+    this.xcor = xcor;
+    setIcon(this.color);
+    new JLabel(getIcon());
+    setVisible(true);
+  }
+  
+  //-------------------Getters and Setters--------------------
+  
+  public int getY(){
+    return ycor;
+  }
+  public int getX(){
+    return xcor;
+  }
+  public String getColor(){
+    return color;
+  }
+  public Icon getIcon(){
+    return icon;
+  }
+  
+  public void setY(int y){
+    ycor = y;
+  }
+  public void setX(int x){
+    // XXX: cor = x;
+  }
+  public void setColor(String c){
+    color = c;
+  }
+  public void setIcon(String c){
+    icon = new ImageIcon("~/blocks/"+c+".png");
+    System.out.println(c);
+  }
+  
+  //------------------Icon Display------------------
+  
 }
--- Grid.java
+++ Grid.java
@@ -6,74 +6,80 @@ import java.util.ArrayList;
 import java.util.*;
 
 public class Grid extends JFrame implements MouseListener{
-
-    private static Container pane;
-    private Block[][] squares;
-    private ArrayList<Block> blockPiece;
-    private boolean selected = false;
-    private Block selectedBlock;
-    public static Border standard = new LineBorder(Color.black);
-
+  
+  private static Container pane;
+  private Block[][] squares;
+  private ArrayList<Block> blockPiece;
+  private boolean selected = false;
+  private Block selectedBlock;
+  public static Border standard = new LineBorder(Color.black);
+  
   public Grid(){
-	
+    
     this.setTitle("Deblocked");
     this.setSize(600,600);//Creates a JFrame size 600 by 600
     this.setLocation(550,150);//Sets the location at 550, 150
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+    
     pane = this.getContentPane();
     pane.setLayout(new GridLayout(8,8));
-
+    
     boolean backgroundColor = false;
     
     Random col = new Random();
+
     squares = new Block[10][10];
+    
     for(int i = 0; i < squares.length;i++){
-	for (int j = 0; j < squares[i].length;j++) {	
-	    Block blocks = new Block(col.nextInt(5), i,j);
-	    
-	    squares[i][j] = blocks;
-	    
-	    blocks.addMouseListener(this);
-	    blocks.setBorder(standard);
-	    blocks.setOpaque(true);
-	    pane.add(blocks);
-	}
+      for (int j = 0; j < squares[i].length;j++) {	
+        Block blocks = new Block(col.nextInt(5), i,j);
+        
+        squares[i][j] = blocks;
+        
+        blocks.addMouseListener(this);
+        blocks.setBorder(standard);
+        blocks.setOpaque(true);
+        pane.add(new JLabel(blocks.getIcon()));
+        pane.setVisible(true);
+      }
     }
   }
-
-
+  
+  
   public void mouseClicked(MouseEvent e){
-      Block a = (Block) e.getSource();
-
-      System.out.println(a.getX());
-      System.out.println(a.getY());
-      System.out.println(a.getColor());
-  }
+    Block a = (Block) e.getSource();
     
+    System.out.println(a.getX());
+    System.out.println(a.getY());
+    System.out.println(a.getColor());
+  }
+  
   public void mouseEntered(MouseEvent e) {
-        
-  }
     
+  }
+  
   public void mouseExited(MouseEvent e) {
-        
-  }
     
+  }
+  
   public void mousePressed(MouseEvent e) {
-        
+    
   }
-    
+  
   public void mouseReleased(MouseEvent e) {
-        
-  } 
-
-    public String toString() {
-	return "";
-    }
     
+  } 
+  
+  public String toString() {
+    return "";
+  }
+
+  
   public static void main (String[] args )
   {
     Grid game = new Grid();
+    JFrame frame = new JFrame("Deblocked");
+    frame.add(pane);
     game.setVisible(true);
     game.setResizable(false);
   }
