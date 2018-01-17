33dfbff249f148b18829a4e781a69047e0589edb
djiang2 <djiang2>
2018-01-16 23:25:14 -0500

    Squares disappear after changing the icons

--- Grid.java
+++ Grid.java
@@ -73,34 +73,6 @@ public class Grid extends JFrame implements MouseListener{
     pane.add(goalPanel);
   }
   
-  /*  public void swap(Piece a, Piece b){
-      curBlock = b;
-      b.setCor(a.getX(),a.getY());
-      a.setCor(b.getX(),a.getY());
-      
-      pane.removeAll();
-      
-      Random col = new Random();
-      
-      for(int i = 0; i < squares.length;i++){
-      for (int j = 0; j < squares[i].length;j++) {	
-      Piece blocks = new Piece(col.nextInt(5),j,i);
-      ImageIcon ic = new ImageIcon(System.getProperty("user.dir") + "/blocks/" + blocks.getColor()+ ".png");
-      
-      JLabel icons = new JLabel(ic);
-      blocks.setCor(j,i);
-      
-      squares[i][j] = blocks;
-      rectangles[i][j] = icons;
-      
-      icons.addMouseListener(this);
-      icons.setBorder(standard);
-      
-      pane.add(icons);
-      }
-      }
-      }*/
-  
   public boolean hasMatch(Piece a, Piece b){
     boolean result = false;
     if(a.getColor().equals(b.getColor())){
@@ -138,94 +110,7 @@ public class Grid extends JFrame implements MouseListener{
     goal = g;
   }
   
-  public void mouseClicked(MouseEvent e){
-    if(moves == 0){
-      amove.setText("Game Over");
-    }else{
-      JLabel a = (JLabel) e.getSource();
-      for (int y = 0; y < squares.length; y++){
-        for (int x = 0; x < squares[y].length; x++){
-          if (a == rectangles[x][y]){
-            System.out.println(squares[x][y].getX());
-            System.out.println(squares[x][y].getColor());
-            //      System.out.println(getMoves());
-            
-            if(!selected){
-              selected = true;
-              colorTemp = squares[x][y].getColor();
-              selectedX = x;
-              selectedY = y;
-              System.out.println(colorTemp);
-            }
-            
-            else{
-              System.out.println("Current:" + squares[x][y].getColor());
-              //System.out.println("Past:" + selectedBlock2.getColor());
-              squares[selectedX][selectedY].setColor(squares[x][y].getColor());
-              //System.out.println("After" + squares[selectedBlock2.getX()][selectedBlock2.getY()].getColor());
-              squares[x][y].setColor(colorTemp);
-              
-              if (!anyCombo()){
-                squares[x][y].setColor(squares[selectedX][selectedY].getColor());
-                squares[selectedX][selectedY].setColor(colorTemp);
-              }
-              
-              colorTemp =  null;
-              
-              //selectedBlock2 = null;
-              selected = false;
-
-              pane.removeAll();
-              makeGrid();
-              pane.add(movePanel);
-              pane.add(scorePanel);
-              pane.add(goalPanel);
-              
-              /*selectedBlock2 = squares[y][x];
-                int tempx = selectedBlock1.getCor().getX(); int tempy = selectedBlock1.getCor().getY();              
-              */
-              
-              /* selectedBlock1.public int getField() {
-                 return field;
-                 getCor().setX(y);
-                 selectedBlock1.getCor().setY(x);
-                 selectedBlock2.public int getField() {
-                 return field;
-                 }etCor().setX(tempx);
-                 selectedBlock2.getCor().setY(tempy);
-                 
-                 JLabel tempicon = rectangles[x][y];
-                 rectangles[x][y] = rectangles[tempx][tempy];
-                 rectangles[tempx][tempy] = tempicon;
-                 
-                 pane.removeAll();
-                 moves = moves - 1;
-                 for (int i = 0; i < squares.length; i++){
-                 for (int j = 0; j < squares[i].length; j++){
-                 pane.add(rectangles[i][j]);
-                 }
-                 }
-                 
-                 pane.add(movePanel);
-                 pane.add(scorePanel);
-                 pane.add(goalPanel);
-                 System.out.println(selectedBlock1.getCor());
-                 System.out.println(selectedBlock2.getCor());
-                 selectedBlock1 = null;
-                 selectedBlock2 = null;
-                 //  System.out.println(hasMatch(selectedBlock,squares[x][y]));
-              //change selected with second click and check chain
-              selected = false;
-              System.out.println(selected);
-              */
-            }
-          }
-        }
-      }
-    }
-    System.out.println("_________________");
-    pane.revalidate();
-  }
+ 
 
   public boolean anyCombo(){
     return (checkHorizontal() || checkVertical()); 
@@ -276,6 +161,52 @@ public class Grid extends JFrame implements MouseListener{
   }
   
   //--MouseListener--//
+   public void mouseClicked(MouseEvent e){
+    if(moves == 0){
+      amove.setText("Game Over");
+    }else{
+      JLabel a = (JLabel) e.getSource();
+      for (int y = 0; y < squares.length; y++){
+        for (int x = 0; x < squares[y].length; x++){
+          if (a == rectangles[x][y]){
+            System.out.println(squares[x][y].getX());
+            System.out.println(squares[x][y].getColor());
+            //      System.out.println(getMoves());
+            
+            if(!selected){
+              selected = true;
+              colorTemp = squares[x][y].getColor();
+              selectedX = x;
+              selectedY = y;
+              System.out.println(colorTemp);
+            } else if(selected){
+              System.out.println("Current:" + squares[x][y].getColor());
+              squares[selectedX][selectedY].setColor(squares[x][y].getColor());
+              squares[x][y].setColor(colorTemp);
+              
+              if (!anyCombo()){
+                squares[x][y].setColor(squares[selectedX][selectedY].getColor());
+                squares[selectedX][selectedY].setColor(colorTemp);
+              }
+              
+              colorTemp =  null;
+              selected = false;
+
+              pane.removeAll();
+              pane.validate();
+              makeGrid();
+              pane.add(movePanel);
+              pane.add(scorePanel);
+              pane.add(goalPanel);
+            }
+          }
+        }
+      }
+    }
+    System.out.println("_________________");
+    pane.revalidate();
+  }
+  
   public void mouseEntered(MouseEvent e) {
     
   }
